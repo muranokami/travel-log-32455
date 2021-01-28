15d53e8da2ae3543f290fbe5dbc6740f9910ea40
muranokami <pikupiku68574.dj@gmail.com>
2021-01-28 14:52:42 +0900

    commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :board_users
+  resources :data_pages
+  get 'hello/index'
+  get 'hello/login_check'
+  devise_for :accounts
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+data = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'zero', 'white', 'black', 'gray', 'red', 'blue', 'green', 'yellow', 'cyan', 'magenta']
+
+for  i in 1..100 do 
+   DataPage.create data:data.sample 
+end
